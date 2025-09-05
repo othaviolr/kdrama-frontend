18b13f1dc30f8bc9748c5635fb6d38586915bf97
othaviolr <othavionogueira2003@gmail.com>
2025-09-04 22:15:53 -0300

    feat: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'KDramaSystem - Acompanhe seus doramas favoritos',
-  description:
-    'Sistema completo para acompanhar, avaliar e descobrir novos doramas coreanos',
+  title: 'KDramaSystem',
+  description: '',
 };
 
 export default function RootLayout({
